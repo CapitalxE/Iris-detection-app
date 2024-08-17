f525285bf9cd463949277053fd831137581a85fd
CapitalxE <elvinthomas40@gmail.com>
2024-08-17 19:14:06 +0530

    Update deply.web.app.py

--- deply.web.app.py
+++ deply.web.app.py
@@ -3,18 +3,6 @@ import pickle
 import streamlit as st
 import requests
 
-model_url = "trained_model.sav"
-
-try:
-    response = requests.get(model_url)
-    if response.status_code == 200:
-        
-        loaded_model = pickle.loads(response.content)
-        print("Model loaded successfully!")
-    else:
-        print(f"Error downloading model. Status code: {response.status_code}")
-except Exception as e:
-    print(f"Error loading model: {e}")
     
 loaded_model = pickle.load(open("trained_model.sav", 'rb'))
 def iris_prediction(input_data):
